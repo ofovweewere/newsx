92bebaaf0fb29bfc514b58026de2d74e50151f0f
ofovweewere <ewereeman@gmail.com>
2023-01-27 03:10:33 -0500

    config fix

--- .history/client/config_20230127024503.js
+++ .history/client/config_20230127024503.js
@@ -1,7 +1,7 @@
 const dev = process.env.NODE_ENV !== "production";
 export const baseUrl = dev
   ? `http://localhost:3000/api`
-  : "https://newsx-phi.vercel.app/api";
+  : "https://your_deployment.server.com";
 export const staticResourceUrl = dev
   ? `http://localhost:3000/uploads/`
-  : "https://newsx-phi.vercel.app/uploads/";
+  : "https://your_deployment.server.com";
